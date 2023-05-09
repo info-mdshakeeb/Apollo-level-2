54236e2066e24a364a73d76d080ff7ad98046dff
itzshakeeb <itzshakeeb@gmail.com>
2023-05-09 19:03:23 +0600

    updated

--- src/generic-function.ts
+++ src/generic-function.ts
@@ -1,36 +1,39 @@
-//Arrow Function
+// //Arrow Function
 
-const createArray = (param: string): string[] => {
-  return [param];
-};
-const createArray1 = <X, Y>(param1: X, param2: Y): [X, Y] => {
-  return [param1, param2];
-};
+// const createArray = (param: string): string[] => {
+//   return [param];
+// };
+// const createArray1 = <X, Y>(param1: X, param2: Y): [X, Y] => {
+//   return [param1, param2];
+// };
 
-function createArray2<X, Y>(param1: X, param2: Y): [X, Y] {
-  return [param1, param2];
-}
+// function createArray2<X, Y>(param1: X, param2: Y): [X, Y] {
+//   return [param1, param2];
+// }
 
-const result1 = createArray1<string, string>("Bangladesh", "I love My Country");
-const result2 = createArray1<boolean, Array<string>>(false, ["USA"]);
+// const result1 = createArray1<string, string>("Bangladesh", "I love My Country");
+// const result2 = createArray1<boolean, Array<string>>(false, ["USA"]);
 
-type Name = { name: string };
+// type Name = { name: string };
 
-const result3 = createArray1<Name, boolean>({ name: "Bangladesh" }, false);
+// const result3 = createArray1<Name, boolean>({ name: "Bangladesh" }, false);
 
-//Spread Operator
+// //Spread Operator
 
-// const newData = {...myInfo ,crush};
-const addMeInMyCrushMind = <T>(myInfo: T) => {
-  const crush = "kate Winslet";
-  const newData = { ...myInfo, crush };
-  return newData;
-};
+// // const newData = {...myInfo ,crush};
+// const addMeInMyCrushMind = <T>(myInfo: T) => {
+//   const crush = "kate Winslet";
+//   const newData = { ...myInfo, crush };
+//   return newData;
+// };
 
-const myInfo = {
-  name: "Persian",
-  age: 100,
-  salary: 100000000,
-};
-const result5 = addMeInMyCrushMind(myInfo);
-result5.
+// const myInfo = {
+//   name: "Persian",
+//   age: 100,
+//   salary: 100000000,
+// };
+// const result5 = addMeInMyCrushMind(myInfo);
+
+const addData = <X, Y>(param1: X, param2: Y): [X, Y] => [param1, param2];
+const result2 = addData<number, number>(12, 12);
+console.log(result2);
